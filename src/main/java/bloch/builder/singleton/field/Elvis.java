fb0dbe8293a29d8bdda59f59934d1612f0b51324
skhalak <skhalak@exadel.com>
2025-12-09 16:28:40 +0200

    add comment

--- src/main/java/bloch/builder/singleton/field/Elvis.java
+++ src/main/java/bloch/builder/singleton/field/Elvis.java
@@ -4,7 +4,9 @@ package bloch.builder.singleton.field;
 public class Elvis {
     public static final Elvis INSTANCE = new Elvis();
 
-    private Elvis() { }
+    private Elvis() {
+        System.out.println("private constructor Elvis");
+    }
 
     public void leaveTheBuilding() {
         System.out.println("Whoa baby, I'm outta here!");
