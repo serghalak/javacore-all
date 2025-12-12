49273523785e24684f1f032f14b91ec8c7cb5805
skhalak <skhalak@exadel.com>
2025-12-12 15:26:47 +0200

    add examples with clone method

--- src/main/java/bloch/builder/singleton/field/Elvis.java
+++ src/main/java/bloch/builder/singleton/field/Elvis.java
@@ -1,8 +1,10 @@
 package bloch.builder.singleton.field;
 
 // Singleton with public final field  (Page 17)
-public class Elvis {
+public class Elvis implements Cloneable {
     public static final Elvis INSTANCE = new Elvis();
+    public int intClone;
+    public String stringClone;
 
     private Elvis() {
         System.out.println("private constructor Elvis");
@@ -16,5 +18,34 @@ public class Elvis {
     public static void main(String[] args) {
         Elvis elvis = Elvis.INSTANCE;
         elvis.leaveTheBuilding();
+        elvis.intClone = 1;
+        elvis.stringClone = "origin Elvis";
+
+        Elvis clone = null;
+        try {
+            clone = (Elvis) elvis.clone();
+            clone.intClone = 2;
+            clone.stringClone = "clone Elvis";
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(elvis == clone);
+        System.out.println(elvis.equals(clone));
+        System.out.println(elvis);
+        System.out.println(elvis.intClone + " " + elvis.stringClone);
+        System.out.println(clone.intClone + " " + clone.stringClone);
+        System.out.println(clone);
     }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        System.out.println("Cloning Elvis");
+        return super.clone();
+    }
+
+//    @Override
+//    protected Elvis clone() throws CloneNotSupportedException {
+//        System.out.println("Cloning Elvis");
+//        return (Elvis) super.clone();
+//    }
 }
