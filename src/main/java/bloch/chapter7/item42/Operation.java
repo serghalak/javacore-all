dfae9d866d190a2f294d956799ce6ebee2b77762
skhalak <skhalak@exadel.com>
2026-02-16 08:50:26 +0200

    add Bloch lamda expressions fix parameters

--- src/main/java/bloch/chapter7/item42/Operation.java
+++ src/main/java/bloch/chapter7/item42/Operation.java
@@ -25,8 +25,8 @@ public enum Operation {
 
     // Main method from Item 34 (Page 163)
     public static void main(String[] args) {
-        double x = Double.parseDouble(args[0]);
-        double y = Double.parseDouble(args[1]);
+        double x = 2.0D; //Double.parseDouble(args[0]);
+        double y = 3.0D; //Double.parseDouble(args[1]);
         for (Operation op : Operation.values())
             System.out.printf("%f %s %f = %f%n",
                     x, op, y, op.apply(x, y));
